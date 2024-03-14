4e11c48c24cc3239af2b2f0708625ac0c8557778
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-03-14 22:07:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementById("greet").style.color="#B0E0E6"
+}
+function night(){
+  document.body.style.backgroundColor="#4682B4";
+}
+function p(){
+  document.getElementById("p").style.color="#8B008B";
+  document.getElementById("p").style.fontFamily="Times New Roman";
+}
